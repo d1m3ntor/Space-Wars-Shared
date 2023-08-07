36803cde1154c2ae37b58ace1bf5e3bcc72300a7
dmytrohryb <dmitrii.9rib@gmail.com>
2023-08-07 10:42:41 +0300

    no message

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Shared.Scripts
 {
-    public abstract class Player : NetworkBehaviour
+    public class Player : NetworkBehaviour
     {
         protected NetworkVariable<Vector2> direction = new (default, 
             NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
